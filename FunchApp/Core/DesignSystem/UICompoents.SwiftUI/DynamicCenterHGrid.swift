3030020f887b8422ce735cc00438b04dd2407582
seongmin221 <seongmin221@naver.com>
2024-01-25 00:20:59 +0900

    added dynamic center h grid

---
+++ FunchApp/Core/DesignSystem/UICompoents.SwiftUI/DynamicCenterHGrid.swift
@@ -0,0 +1,18 @@
+//
+//  DynamicCenterHGrid.swift
+//  FunchApp
+//
+//  Created by 이성민 on 1/24/24.
+//
+
+import SwiftUI
+
+struct DynamicCenterHGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DynamicCenterHGrid()
+}
