50320ebba0ad30cd79f9b4851f63b54f15497cdd
seongmin221 <seongmin221@naver.com>
2024-01-24 23:35:28 +0900

    added match result needed views

---
+++ FunchApp/Presentation/MatchResultScene/MatchResultConversationView.swift
@@ -0,0 +1,18 @@
+//
+//  MatchResultConversationView.swift
+//  FunchApp
+//
+//  Created by 이성민 on 1/23/24.
+//
+
+import SwiftUI
+
+struct MatchResultConversationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchResultConversationView()
+}
---
+++ FunchApp/Presentation/MatchResultScene/MatchResultOpponentView.swift
@@ -0,0 +1,18 @@
+//
+//  MatchResultOpponentView.swift
+//  FunchApp
+//
+//  Created by 이성민 on 1/24/24.
+//
+
+import SwiftUI
+
+struct MatchResultOpponentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchResultOpponentView()
+}
